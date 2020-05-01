4077648b39a418b5b5f0930418c540e9d7c6b3d6
Dheeraj Singla <dheeraj.singla@caastle.com>
2020-05-01 12:51:43 +0530

    xyz

--- Sources/MathOperations/MathOperations.swift
+++ Sources/MathOperations/MathOperations.swift
@@ -28,6 +28,10 @@ public func multiply(num1: String, num2: String) {
     print("multiplying \(num1) and \(num2) gives: \(product)")
 }
 
+public func createfile(at path: String) {
+    try Folder.createFileIfNeeded(at: path)
+}
+
 public let swiftyml: String = """
 disabled_rules: # rule identifiers to exclude from running
 - cyclomatic_complexity
